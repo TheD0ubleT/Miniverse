d1391bb810133465979cd78f0136094978bc4a0c
Stephane Latil <Stephanelatil@gmail.com>
2016-04-19 09:55:04 +0200

    Fixed gravitational pull, again

--- src/sim/Maths/Balistics/ComplexBalistics.java
+++ src/sim/Maths/Balistics/ComplexBalistics.java
@@ -52,6 +52,26 @@ public class ComplexBalistics
 		}
 	}
 	
+	public void updateOnePosition(int objectIndex, double timeStep)
+	{
+		int i = objectIndex;
+		Vector acc = new Vector();
+		for (int j = 0; j < astronomicalObjects.length; j++) // for each Other object
+		{
+			if (i != j)
+			{
+				acc = Operators.vectorAdd(acc, new Vector(
+						getGravitationalPull(astronomicalObjects[i], astronomicalObjects[j], 'x'),
+						getGravitationalPull(astronomicalObjects[i], astronomicalObjects[j], 'y'),
+						getGravitationalPull(astronomicalObjects[i], astronomicalObjects[j], 'z')));
+			}
+		}
+		astronomicalObjects[i].setCoordinates(new Coordinates(
+				calculateNewPosOnAxis('x', timeStep, acc.getCoordinates().getX(), astronomicalObjects[i].getSpeed().getX(), astronomicalObjects[i].getCoordinates().getX()),
+				calculateNewPosOnAxis('y', timeStep, acc.getCoordinates().getY(), astronomicalObjects[i].getSpeed().getY(), astronomicalObjects[i].getCoordinates().getY()),
+				calculateNewPosOnAxis('z', timeStep, acc.getCoordinates().getZ(), astronomicalObjects[i].getSpeed().getZ(), astronomicalObjects[i].getCoordinates().getZ())));
+	}
+	
 	public double calculateNewPosOnAxis(char axis, double timeStep, double acceleration, double currentSpeed, double currentPosition)
 	{
 		// using newton's second law:
@@ -64,11 +84,11 @@ public class ComplexBalistics
 	private double getGravitationalPull(AstronomicalObject obj, AstronomicalObject obj2, char axis)
 	{
 		if (axis == 'x' && (obj2.getCoordinates().getX() - obj.getCoordinates().getX()) != 0)
-			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getX() - obj.getCoordinates().getX()) * Math.signum(-obj2.getCoordinates().getX() + obj.getCoordinates().getX());
+			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getX() - obj.getCoordinates().getX()) * Math.signum(obj2.getCoordinates().getX() - obj.getCoordinates().getX());
 		if (axis == 'y' && (obj2.getCoordinates().getY() - obj.getCoordinates().getY()) != 0)
-			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getY() - obj.getCoordinates().getY()) * Math.signum(-obj2.getCoordinates().getX() + obj.getCoordinates().getX());
+			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getY() - obj.getCoordinates().getY()) * Math.signum(obj2.getCoordinates().getX() - obj.getCoordinates().getX());
 		if (axis == 'z' && (obj2.getCoordinates().getZ() - obj.getCoordinates().getZ()) != 0)
-			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getZ() - obj.getCoordinates().getZ()) * Math.signum(-obj2.getCoordinates().getX() + obj.getCoordinates().getX());
+			return (Constants.G * obj2.getMass()) / Operators.square(obj2.getCoordinates().getZ() - obj.getCoordinates().getZ()) * Math.signum(obj2.getCoordinates().getX() - obj.getCoordinates().getX());
 		
 		return 0d;
 	}

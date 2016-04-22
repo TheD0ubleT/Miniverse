dca19b5086a5b99a4a131bfc1d63b2c216dba045
Stephane Latil <Stephanelatil@gmail.com>
2016-04-22 17:28:19 +0200

    Added some fun Methods for when bored

---
+++ src/sim/ForTheLolz.java
@@ -0,0 +1,21 @@
+package sim;
+
+// sometimes I believe compiler ignores all my comments
+// total_hours_wasted_fixing_nothing = 2;
+
+public final class ForTheLolz
+{
+	
+	//Crashed program (due to StackOverflow)
+	//Because why not
+	public final static int crash()
+	{
+		return crash();
+	}
+	
+	//returns 1, for some reason
+	public int return1()
+	{
+		return (byte) + (char) - (int) + (long) - 1;
+	}
+}

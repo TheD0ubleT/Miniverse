05be5e9aeda03c3883d0fb49ac3a0c631f4ea736
Stephane Latil <Stephanelatil@gmail.com>
2016-04-23 09:44:11 +0200

    Fixed units and useless memory usage/variable initialisation
    Everything must stay in the same units : length in meters
    Units respect the international system of units

--- src/sim/Maths/Gravitation.java
+++ src/sim/Maths/Gravitation.java
@@ -2,30 +2,23 @@ package sim.Maths;
 
 import sim.World.Space.AstronomicalObjects.Planet;
 
-public class Gravitation {
-    public static double SurfaceGravity(double mass, double radius) {
-    	double surfaceGravity = 0;
-    	radius *= 1000; //Convert to meters
-    	surfaceGravity = (sim.Constants.G * mass) / sim.Maths.Operators.square(radius);
-    	return surfaceGravity;
+public class Gravitation
+{
+    public static double SurfaceGravity(double mass, double radius)
+    {
+    	return (sim.Constants.G * mass) / sim.Maths.Operators.square(radius);
         //a = (G*M)/r^2
     }
     
-    public static double SurfaceGravity(Planet planet) {
-    	double surfaceGravity = 0;
-    	double mass = planet.getMass();
-    	double radius = planet.radius;
-    	
-    	radius *= 1000; //Convert to meters
-    	surfaceGravity = (sim.Constants.G * mass) / sim.Maths.Operators.square(radius);
-    	return surfaceGravity;
+    public static double SurfaceGravity(Planet planet)
+    {    	
+    	return (sim.Constants.G * planet.getMass()) / sim.Maths.Operators.square(planet.getRadius());
         //a = (G*M)/r^2
     }
     
-    public static double gravitationalForceBetweenObjects(double mass_one, double mass_two, double distance) {
-    	double forceGravity = 0;
-    	forceGravity = (sim.Constants.G * mass_one * mass_two) / (distance * distance);  
-    	return forceGravity;
+    public static double gravitationalForceBetweenObjects(double mass_one, double mass_two, double distance)
+    {
+    	return (sim.Constants.G * mass_one * mass_two) / (distance * distance);
         //F = G*m1*m2 /r^2
     }
 }

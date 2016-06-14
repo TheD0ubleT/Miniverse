ca4aba2d2e0b7bd1cba5a6eab415bc985334d700
Kenny <kennythakoala@gmail.com>
2016-06-14 01:50:30 -0400

    Added unfinished Cylinder class
    Volume, Surface Area, Lateral Area, Face Area for a Cylinder.
    **Might be a good idea to add seperate package for simple area
    calculations such as pi*square(r)

---
+++ src/sim/Maths/Volume/Cylinder.java
@@ -0,0 +1,50 @@
+package sim.Maths.Volume;
+
+import sim.Constants;
+import sim.Maths.Operators;
+import sim.World.Space.Coordinates;
+
+public class Cylinder
+{
+	private Coordinates centerOfInertia;
+	private double radius;
+	private double height;
+	
+	public Cylinder(Coordinates centerOfInertia, double radius)
+	{
+		this.radius = radius;
+		this.centerOfInertia = centerOfInertia;
+	}
+	
+	////Getters////
+	public Coordinates getCenterOfInertia()
+	{
+		return this.centerOfInertia;
+	}
+	
+	public double getVolume() //Volume
+	{
+		return getFaceArea() * height;
+	}
+	
+	public double getLateralArea() //For lateral side without faces
+	{
+		return 2 * Constants.PI * radius * height;
+	}
+	
+	public double getFaceArea() //For faces (top or bottom)
+	{
+		return Constants.PI * Operators.square(radius);
+	}
+	
+	public double getSurfaceArea() //For Surface area (Lateral + 2*Face)
+	{
+		return getLateralArea() + 2 * Constants.PI * Operators.square(radius);
+	}
+	
+	////Setters////
+	public void setCenterOfInertia(Coordinates centerOfInertia)
+	{
+		this.centerOfInertia = centerOfInertia;
+	}
+}

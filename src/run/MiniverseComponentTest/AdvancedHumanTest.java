beeb8f94aa330df8552cc7155c6316077ae18de8
Kenny <kennythakoala@gmail.com>
2016-06-15 19:28:21 -0400

    Fixed HumanRandom, re arranged some variables, removed unneeded code

--- src/run/MiniverseComponentTest/AdvancedHumanTest.java
+++ src/run/MiniverseComponentTest/AdvancedHumanTest.java
@@ -7,12 +7,14 @@ public class AdvancedHumanTest
 	public static void main(String[] args)
 	{
 		Human people = new Human();
+		people.HumanRandom();
 		System.out.println("/-=-=-=NEW HUMAN=-=-=-+"); //23
 		System.out.printf("%2s %s", "",people.getFirstName() + " " + people.getLastName() + "\n");
 		System.out.printf("%2s %s", "",people.getHumanHash() + "\n");
 		System.out.printf("%2s %s", "",people.getAge() + "\n");
 		System.out.printf("%2s %s", "",people.getGender() + "\n");
 		System.out.println("+-=-=-=-=-=-=-=-=-=-=-/");
-		System.out.println(people.getHumanNamesIntersexLength());
+		System.out.println(people.toString());
+		System.out.println(people.getHumanFirstNamesIntersexLength());
 	}
 }
--- src/sim/Organism/Person/Human.java
+++ src/sim/Organism/Person/Human.java
@@ -9,17 +9,17 @@ public class Human
 {
 	private static ArrayList<Long> allHashes;
 	private long humanHash;
-	public String gender;
-	public int age;
-	public String birthday;
-	public String firstName;
-	public String lastName;
-	
+	private String gender;
+	private int age;
+	private String birthday;
+	private String firstName;
+	private String lastName;
+	//private public
 	public static String[] HumanGenders = {"Male", "Female", "Intersex"};
 	public static String[] HumanFirstNamesMale = {"Kenny", "Andrew", "Michael", "Quade"};//TODO: make a text file of thousands of names and instead make in reference text file
 	public static String[] HumanFirstNamesFemale = {"Mary", "Jordyn", "Amanda", "Sydney"};
 	public static String[] HumanFirstNamesIntersex = new String[HumanFirstNamesMale.length + HumanFirstNamesFemale.length];
- 	public static String[] HumanLastNames = {"Smith", "Miller", "Tibboles", "Craun", };
+	public static String[] HumanLastNames = {"Smith", "Miller", "Tibboles", "Craun", };
 	public static int[] HumanAges = new int[122];
 	
 	////Constructor////
@@ -61,6 +61,10 @@ public class Human
 		while (allHashes.contains(temp));
 
 		this.humanHash = temp;
+	
+		for (int i = 0; i < HumanAges.length; i++) {
+			HumanAges[i] = i + 1;
+		}
 		
 		this.age = age;
 		this.gender = gender;
@@ -71,21 +75,6 @@ public class Human
 	////RANDOM HUMAN////
 	public void HumanRandom()
 	{
-		allHashes = new ArrayList<Long>();
-		//only create new hash array if none already made, avoids overwriting previous
-		allHashes = allHashes == null ? new ArrayList<Long>(): allHashes;
-		long temp = 0;
-		do
-		{
-			temp = Constants.RAND.nextLong();
-		}
-		while (allHashes.contains(temp));
-		
-		for (int i = 0; i < HumanAges.length; i++) {
-			HumanAges[i] = i + 1;
-		}
-		
-		this.humanHash = temp;
 		this.gender = HumanGenders[Constants.RAND.nextInt(HumanGenders.length)];
 		this.age = HumanAges[Constants.RAND.nextInt(HumanAges.length)];
 		if(getGender() == "Male")
@@ -97,10 +86,10 @@ public class Human
 			for (int i = 0; i < HumanFirstNamesMale.length; i++) {
 				HumanFirstNamesIntersex[i] = HumanFirstNamesMale[i];
 			}
-			for (int i = HumanFirstNamesMale.length; i < HumanFirstNamesFemale.length; i++) {
-				HumanFirstNamesIntersex[i] = HumanFirstNamesFemale[i];
+			for (int i = HumanFirstNamesMale.length; i < HumanFirstNamesIntersex.length; i++) {
+				HumanFirstNamesIntersex[i] = HumanFirstNamesFemale[i - HumanFirstNamesMale.length];
 			}
-			this.firstName = HumanFirstNamesIntersex[Constants.RAND.nextInt(HumanFirstNamesIntersex.length)];
+			this.firstName = HumanFirstNamesIntersex[Constants.RAND.nextInt(HumanFirstNamesIntersex.length - 1)];
 		}
 		this.lastName = HumanLastNames[Constants.RAND.nextInt(HumanLastNames.length)];
 	}
@@ -136,11 +125,11 @@ public class Human
 		return this.lastName;
 	}
 	
-	public int getHumanNamesIntersexLength()
+	public int getHumanFirstNamesIntersexLength()
 	{
 		return HumanFirstNamesIntersex.length;
 	}
-	
+
 	////Setters////
 	public void setGender(String gender)
 	{
